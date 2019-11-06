d7e424b7cd5ee40804e0e80e87bdc14d51d3f688
Duyet Le <lvduyet@fossil.com>
2019-11-06 11:23:56 +0700

    Update

--- flask/app.py
+++ flask/app.py
@@ -1,62 +1,73 @@
-import os
 import json
-import requests
-import gensim
+import os
 import pickle
 
 import numpy as np
+import requests
 from sklearn.metrics.pairwise import cosine_similarity
-from underthesea import word_tokenize
-from gensim.models import LsiModel
+
+import gensim
+from flask import Flask, jsonify, render_template, request
 from gensim.corpora import Dictionary
-
-from flask import Flask, request, jsonify, render_template
-
+from gensim.models import LsiModel
+from loguru import logger
+from underthesea import word_tokenize
 
 app = Flask(__name__)
 
-data_path = '/Users/admin/personal/master/ir/news_dataset/'
-w2v_path = 'baomoi.model.bin'
-lsi_model_path = 'lsi_model.pickle'
-name_index_mapping_path = 'doc_name_index_mapping.pickle'
+current_dir = os.path.abspath(os.path.dirname(__file__))
+data_path = os.path.join(current_dir, 'news_dataset')
+w2v_path = os.path.join(current_dir, 'model/baomoi.model.bin')
+lsi_model_path = os.path.join(current_dir, 'model/lsi_model.pickle')
+name_index_mapping_path = os.path.join(
+    current_dir, 'model/doc_name_index_mapping.pickle')
 
-############### expansion
-w2v_model = gensim.models.KeyedVectors.load_word2vec_format(w2v_path, binary=True)
+# expansion
+w2v_model = gensim.models.KeyedVectors.load_word2vec_format(
+    w2v_path, binary=True)
+logger.info(f'Loaded {w2v_path}')
+
 
 def expand_query(query):
     mean_vec = np.zeros(w2v_model.vector_size)
     for q in query.split():
         if q in w2v_model:
-            mean_vec +=w2v_model[q]
+            mean_vec += w2v_model[q]
 
     result = w2v_model.similar_by_vector(mean_vec, 5)
     result = [term for term, score in result if score > 0.6]
     return result
 
-############### LSI core
+
+# LSI core
 with open(lsi_model_path, 'rb') as handle:
+    logger.info(f'Loaded {lsi_model_path}')
     dictionary, vectorized_corpus, model = pickle.load(handle)
 
 with open(name_index_mapping_path, 'rb') as handle:
+    logger.info(f'Loaded {name_index_mapping_path}')
     index2name, name2index = pickle.load(handle)
 
-def query_corpus(query, use_expansion=False):
+
+def query_corpus(query, use_expansion=False, top_n=20):
     expansion = []
     query_ = word_tokenize(query, 'text').lower()
     if use_expansion:
         expansion = expand_query(query_)
-    
+
     query_bow = dictionary.doc2bow(query_.split() + expansion)
     query_lsi = np.array([v for i, v in model[query_bow]])
-    
-    result = list(enumerate(cosine_similarity([query_lsi], vectorized_corpus)[0]))
-    result = sorted(result, key=lambda x:x[1], reverse=True)[:20]
+
+    result = list(enumerate(cosine_similarity(
+        [query_lsi], vectorized_corpus)[0]))
+    result = sorted(result, key=lambda x: x[1], reverse=True)[:top_n]
     result = [(index2name[i], score) for i, score in result]
     return result
 
+
 def name2content(doc_name):
-    content=''
-    with open(data_path + doc_name, 'r', encoding='utf-16', errors="ignore") as f:
+    content = ''
+    with open(os.path.join(data_path, doc_name), 'r', encoding='utf-16', errors="ignore") as f:
         content = f.read()
     return content
 
@@ -65,11 +76,13 @@ def name2content(doc_name):
 def index():
     return render_template('index.html')
 
+
 @app.route('/search', methods=['POST', 'GET'])
 def search():
     try:
-        query = request.args.get('query')
-        print(query)
+        query = request.json.get('query')
+        logger.info(query)
+
         result_no_expand = query_corpus(query, False)
         result_no_expand = [name2content(r) for r, s in result_no_expand]
         result_expand = query_corpus(query, True)
@@ -81,14 +94,16 @@ def search():
         }
 
         return jsonify(result=result)
-    except:
-        return None
+    except Exception as e:
+        logger.exception(e)
+        return jsonify(err=1)
 
 
 if __name__ == '__main_':
     try:
-        print('run server')
-        app.run(debug=True, port=8081, host='0.0.0.0')
+        host = os.getenv('HOST', '0.0.0.0')
+        port = os.getenv('PORT', 8081)
+        logger.info(f'Started server at http://{host}:{port}')
+        app.run(debug=True, port=port, host=host)
     except:
-        print('sc stop')
-        sc.stop()
+        logger.warning('server stopped')
